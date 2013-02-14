f98c597f8e7216b1f40375d2b0aef8613055f6d1
Zurc <cruzjurado@gmail.com>
2013-02-14 07:44:39 -0800

    Create front_end.php

---
+++ front_end.php
@@ -0,0 +1,23 @@
+<?php include '/Applications/MAMP/htdocs/prueba6/includes/header.php';?>
+
+
+    <div id="content">
+    
+      <div class="text-wrapper">
+            <h1>FRONT END DEVELOPER</h1>
+            <p>I love clean code.</p>
+            <p>I have knowledge of HTML/5, CSS/3, JavaScript (jQuery), CMS (Wordpress, Joomla)</p>
+            <p>Learning PHP...</p>
+            <p>&nbsp;</p>
+            <p>If you want to see some of my code, you can visit me on 
+                <a title="Github" href="https://github.com/Zurc/Cruz-Jurado--personal-web-">
+                    Github
+                </a>
+            </p>
+            
+        </div>
+        
+        
+    </div>
+      
+<?php include '/Applications/MAMP/htdocs/prueba6/includes/footer.php';?>
