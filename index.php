c9a97ae3af1937eaf822aee56f75639a923560c6
Zurc <cruzjurado@gmail.com>
2013-02-14 15:32:12 +0000

    Update index.php

--- index.php
+++ index.php
@@ -1,38 +1,43 @@
 <?php include '/Applications/MAMP/htdocs/prueba6/includes/header.php';?>
 
-<div id="content">
-  <!--        <a href="index.html"><p></p></a>
-        <h1>WELCOME TO MY SITE!</h1>
--->        
-  <p>What can I do for you?</p>
-  <p>I'm here because...</p>
-  <br>
-    <div class="col">
-      <a  href="#" alt="web design">
-        <img class="wd" src="assets/img/choice_button_wd.png">
-      </a>
-      <a>
-        <img class="wd" src="assets/img/choice_button_fed.png">
-      </a>
-      <a>
-        <img class="wd" src="assets/img/choice_button_uid.png">
-      </a>
-      <a>
-        <img class="wd" src="assets/img/choice_button_ill.png">
-      </a>
-    </div>
-  <br>
-  <br>
-  <br>
-  <p>&nbsp;</p>
-  <br>
-  <p>Other reason...</p>
-  <br>
-  <br>
-  <br>
-  <br>        <br>
-  <p>&nbsp;</p>
-</div>
 
+    <div id="content">
+    
+        <div class="text-wrapper">
+            <h1>WEB DESIGN</h1>
+            <p>I would love to Design for you a <span>unique</span> web site, based on your preferences and necessities:</p>
+        </div>
+        
+        <div id="wrapper">    
+            <div class="callbacks_container">
+                <ul class="rslides" id="slider_wd">
+                  <li>
+                    <img src="assets/img/img1.jpg" alt="">
+                    <p class="caption">This is a caption</p>
+                  </li>
+                  <li>
+                    <img src="assets/img/img2.jpg" alt="">
+                    <p class="caption">This is another caption</p>
+                  </li>
+                  <li>
+                    <img src="assets/img/img3.jpg" alt="">
+                    <p class="caption">The third caption</p>
+                  </li>
+                  <li>
+                    <img src="assets/img/img4.jpg" alt="">
+                    <p class="caption">The third caption</p>
+                  </li>
+                  <li>
+                    <img src="assets/img/img5.jpg" alt="">
+                    <p class="caption">The third caption</p>
+                  </li>
+                  <li>
+                    <img src="assets/img/img6.jpg" alt="">
+                    <p class="caption">The third caption</p>
+                  </li>
+                </ul>
+            </div>
+        </div>
+    </div>
       
 <?php include '/Applications/MAMP/htdocs/prueba6/includes/footer.php';?>
