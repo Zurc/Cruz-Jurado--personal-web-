3bfd96d934de0fc726a9adc8eb72f2d70c29044b
Zurc <cruzjurado@gmail.com>
2013-02-14 06:29:02 -0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,38 @@
+<?php include '/Applications/MAMP/htdocs/prueba6/includes/header.php';?>
+
+<div id="content">
+  <!--        <a href="index.html"><p></p></a>
+        <h1>WELCOME TO MY SITE!</h1>
+-->        
+  <p>What can I do for you?</p>
+  <p>I'm here because...</p>
+  <br>
+    <div class="col">
+      <a  href="#" alt="web design">
+        <img class="wd" src="assets/img/choice_button_wd.png">
+      </a>
+      <a>
+        <img class="wd" src="assets/img/choice_button_fed.png">
+      </a>
+      <a>
+        <img class="wd" src="assets/img/choice_button_uid.png">
+      </a>
+      <a>
+        <img class="wd" src="assets/img/choice_button_ill.png">
+      </a>
+    </div>
+  <br>
+  <br>
+  <br>
+  <p>&nbsp;</p>
+  <br>
+  <p>Other reason...</p>
+  <br>
+  <br>
+  <br>
+  <br>        <br>
+  <p>&nbsp;</p>
+</div>
+
+      
+<?php include '/Applications/MAMP/htdocs/prueba6/includes/footer.php';?>
