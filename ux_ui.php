e82e34b19048fb42ab5894457999f561d3793431
Zurc <cruzjurado@gmail.com>
2013-02-14 08:10:54 -0800

    Create ux_ui.php

---
+++ ux_ui.php
@@ -0,0 +1,32 @@
+<?php include 'includes/header.php';?>
+
+    <div id="content">
+    
+          <div class="text-wrapper">
+              <h1>USER EXPERIENCE | USER INTERFACE</h1>
+              <p>I will conceptualize the User Experience/Interface of your web platform.</p>
+              <p>Thinking in Design and Usability, but also the emotions and feelings generated in the user.</p>
+              <p>My aim is to create for you a very suggestive, definitely inspiring visual user interfaces.</p>
+          </div>
+          
+          <div id="wrapper">    
+              <div class="callbacks_container">
+                  <ul class="rslides" id="slider_wd">
+                        <li>
+                          <img src="assets/img/img1.jpg" alt="">
+                          <p class="caption">This is a caption</p>
+                        </li>
+                        <li>
+                          <img src="assets/img/img2.jpg" alt="">
+                          <p class="caption">This is another caption</p>
+                        </li>
+                        <li>
+                          <img src="assets/img/img3.jpg" alt="">
+                          <p class="caption">The third caption</p>
+                        </li>
+                  </ul>
+              </div>
+          </div>
+    </div>
+      
+<?php include 'includes/footer.php';?>
