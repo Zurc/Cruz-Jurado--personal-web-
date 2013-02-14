0c996a000574e1d3416e22421268404847ae8911
Zurc <cruzjurado@gmail.com>
2013-02-14 07:35:48 -0800

    Create web_design.php

---
+++ web_design.php
@@ -0,0 +1,43 @@
+<?php include '/Applications/MAMP/htdocs/prueba6/includes/header.php';?>
+
+
+    <div id="content">
+    
+        <div class="text-wrapper">
+            <h1>WEB DESIGN</h1>
+            <p>I would love to Design for you a <span>unique</span> web site, based on your preferences and necessities:</p>
+        </div>
+        
+        <div id="wrapper">    
+            <div class="callbacks_container">
+                <ul class="rslides" id="slider_wd">
+                  <li>
+                    <img src="assets/img/img1.jpg" alt="">
+                    <p class="caption">This is a caption</p>
+                  </li>
+                  <li>
+                    <img src="assets/img/img2.jpg" alt="">
+                    <p class="caption">This is another caption</p>
+                  </li>
+                  <li>
+                    <img src="assets/img/img3.jpg" alt="">
+                    <p class="caption">The third caption</p>
+                  </li>
+                  <li>
+                    <img src="assets/img/img4.jpg" alt="">
+                    <p class="caption">The third caption</p>
+                  </li>
+                  <li>
+                    <img src="assets/img/img5.jpg" alt="">
+                    <p class="caption">The third caption</p>
+                  </li>
+                  <li>
+                    <img src="assets/img/img6.jpg" alt="">
+                    <p class="caption">The third caption</p>
+                  </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+      
+<?php include '/Applications/MAMP/htdocs/prueba6/includes/footer.php';?>
