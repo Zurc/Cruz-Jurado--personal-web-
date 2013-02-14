0cbd2c133a27e0e4c4944c1242f66c1eae20fe8a
Zurc <cruzjurado@gmail.com>
2013-02-14 16:00:16 +0000

    Update index.php

--- index.php
+++ index.php
@@ -1,75 +1,35 @@
-<!doctype html>
-<html>
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
-    
-    <title>Prueba Web Cruz</title>
-    
-    <link href="../css/reset.css" rel="stylesheet" type="text/css">
-    <link href="main.css" rel="stylesheet" type="text/css">
-    
-    <link href='http://fonts.googleapis.com/css?family=Quicksand' rel='stylesheet' type='text/css'>
-    <link href='http://fonts.googleapis.com/css?family=Advent+Pro:300,400' rel='stylesheet' type='text/css'>
-    
-    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
-	<script src="assets/js/responsiveslides.min.js"></script>
-    <script>
-    // You can also use "$(window).load(function() {"
-    $(function () {
+<?php include 'includes/header.php';?>
 
-      $("#slider_wd").responsiveSlides({
-        auto: false,
-        pager: false,
-        nav: true,
-        speed: 500,
-        namespace: "callbacks",
-        before: function () {
-          $('.events').append("<li>before event fired.</li>");
-        },
-        after: function () {
-          $('.events').append("<li>after event fired.</li>");
-        }
-      });
-
-    });
-  </script>
-        
-</head>
-
-<body>
-
-	<header>
-    	<h1 >
-        	<a title="Cruz-Jurado" href="index.php">
-            	Cruz Jurado
+<div id="content">
+      <p>What can I do for you?</p>
+      <p>I'm here because...</p>
+      <br>
+      <div class="col">
+            <a  href="web_design.php" alt="web design">
+                <img class="wd" src="assets/img/choice_button_wd.png">
             </a>
-        </h1>
-        <h2>
-        	<a title="Creativity-at-your-service" href="index.php">
-            	Creativity at your service
+            <a>
+                <img class="wd" src="assets/img/choice_button_fed.png">
             </a>
-         </h2>
-    </header>  
-               
-    <nav>
-          <ul>
-              <li>
-                  <a href="web_design.php" alt="web design">Web Design</a>
-                 </li>
-              <li>
-                  <a href="front_end.php" alt="front end developer">Front End</a>
-                 </li>
-              <li>
-                  <a href="ux_ui.php" alt="user experience / user interface">UX/UI</a>
-                 </li>           
-              <li>
-                  <a href="illust.php" alt="illustrator">Illustration</a>
-                 </li>
-    <!--          <li>
-                </li>
-              ...   
-    -->             
-             </ul>
-    </nav> 
+            <a>
+                <img class="wd" src="assets/img/choice_button_uid.png">
+            </a>
+            <a>
+                <img class="wd" src="assets/img/choice_button_ill.png">
+            </a>
+      </div>
+      <br>
+      <br>
+      <br>
+      <p>&nbsp;</p>
+      <br>
+      <p>Other reason...</p>
+      <br>
+      <br>
+      <br>
+      <br>        <br>
+      <p>&nbsp;</p>
+</div>
+
+      
+<?php include 'includes/footer.php';?>
