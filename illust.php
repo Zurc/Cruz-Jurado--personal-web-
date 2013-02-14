95158963beefa8da54e20be5d5f55031133213a4
Zurc <cruzjurado@gmail.com>
2013-02-14 08:28:04 -0800

    Create illust.php

---
+++ illust.php
@@ -0,0 +1,32 @@
+<?php include 'includes/header.php';?>
+
+    <div id="content">
+    
+        <div class="text-wrapper">
+              <h1>ILLUSTRATION</h1>
+              <p>Creating things is one of my passions. Telling a story, recreating...</p>
+              <p>I can be very reallistic if I need to.</p>
+              <p>Tell me your dream, we will make it real.</p>
+        </div>
+        
+        <div id="wrapper">    
+              <div class="callbacks_container">
+                  <ul class="rslides" id="slider_wd">
+                      <li>
+                        <img src="assets/img/img1.jpg" alt="">
+                        <p class="caption">This is a caption</p>
+                      </li>
+                      <li>
+                        <img src="assets/img/img2.jpg" alt="">
+                        <p class="caption">This is another caption</p>
+                      </li>
+                      <li>
+                        <img src="assets/img/img3.jpg" alt="">
+                        <p class="caption">The third caption</p>
+                      </li>
+                  </ul>
+              </div>
+        </div>
+    </div>
+      
+<?php include 'includes/footer.php';?>
