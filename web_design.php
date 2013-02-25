847e0f7e0b8a3966c119c9e11f71245367c5cb33
Zurc <cruzjurado@gmail.com>
2013-02-25 17:15:26 +0000

    Update web_design.php

--- web_design.php
+++ web_design.php
@@ -13,31 +13,28 @@
                 <ul class="rslides" id="slider_wd">
                   <li>
                     <img src="assets/img/img1.jpg" alt="">
-                    <p class="caption">This is a caption</p>
+                    <!--<p class="caption">You can put a caption on the image</p>-->
                   </li>
                   <li>
                     <img src="assets/img/img2.jpg" alt="">
-                    <p class="caption">This is another caption</p>
                   </li>
                   <li>
                     <img src="assets/img/img3.jpg" alt="">
-                    <p class="caption">The third caption</p>
                   </li>
                   <li>
                     <img src="assets/img/img4.jpg" alt="">
-                    <p class="caption">The third caption</p>
                   </li>
                   <li>
                     <img src="assets/img/img5.jpg" alt="">
-                    <p class="caption">The third caption</p>
                   </li>
                   <li>
                     <img src="assets/img/img6.jpg" alt="">
-                    <p class="caption">The third caption</p>
                   </li>
                 </ul>
             </div>
+            
         </div>
+        
     </div>
       
 <?php include '/Applications/MAMP/htdocs/prueba6/includes/footer.php';?>
