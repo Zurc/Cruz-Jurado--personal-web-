16fc2993f6ac998c7c46be89f5a9872c4630ffbf
Zurc <cruzjurado@gmail.com>
2013-02-25 17:16:45 +0000

    Update illust.php

--- illust.php
+++ illust.php
@@ -14,19 +14,18 @@
                   <ul class="rslides" id="slider_wd">
                       <li>
                         <img src="assets/img/img1.jpg" alt="">
-                        <p class="caption">This is a caption</p>
                       </li>
                       <li>
                         <img src="assets/img/img2.jpg" alt="">
-                        <p class="caption">This is another caption</p>
                       </li>
                       <li>
                         <img src="assets/img/img3.jpg" alt="">
-                        <p class="caption">The third caption</p>
                       </li>
                   </ul>
               </div>
+              
         </div>
+        
     </div>
       
 <?php include 'includes/footer.php';?>
